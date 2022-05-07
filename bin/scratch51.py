5427e3f27f29f296b07b34b5e3584f78c8a6800d
Milos Rancic <millosh@gmail.com>
2022-05-07 21:29:52 +0200

    .

--- bin/scratch51.py
+++ bin/scratch51.py
@@ -18,31 +18,45 @@ class MyClient(discord.Client):
         if message.author.id == self.user.id:
             return
 
-        messages = re.split("^- ",open(messages_fd).read())[1:]
-        for message in messages:
-            trigger = message[0]
-            content = message[1:]
+        msgs = re.split("(?:\n|^)- ",open(messages_fd).read())[1:]
+        for msg in msgs:
+            msg = msg.split("\n")
+            trigger = msg[0]
+            content = "\n".join(msg[1:])
             if message.content.startswith(trigger):
                 await message.channel.send(content)
 
-#        actions = json.load(actions.json)
-#        if message.content.startswith('$quiz1'):
-#            no_correct = 0
-#            no_questions = len(list(qas.keys()))
-#            for question in qas:
-#                await message.channel.send(question)
-#                def is_correct(m):
-#                    return m.author == message.author
-#                answer = qas[question]
-#                try:
-#                    guess = await self.wait_for('message', check=is_correct, timeout=30.0)
-#                except asyncio.TimeoutError:
-#                    return await message.channel.send('No answer, no fun! You took too long...'.format(answer))
-#                if guess.content == answer:
-#                    no_correct += 1
-#            await message.channel.send("You answered correctly " + str(no_correct) + " questions of " + str(no_questions) + "!")
-#
-#        if message.content.startswith('$help'):
+        # dictionary  "actions"
+        actions = json.loads(open(actions_fd).read())
+        for act in actions:
+            action = actions[act]
+            trigger = action['keyword']
+            action_file = conf_dir + "/" + action['config']
+            if message.content.startswith(trigger):
+                if action['type'] == 'quiz':
+                    # dictionary "quiz"
+                    #qf = open(action_file).read()
+                    quiz = json.loads(open(action_file).read())
+                    questions = quiz['questions']
+                    no_correct = 0
+                    no_questions = len(list(questions.keys()))
+                    no_points = 0
+                    max_points = 0
+                    for question in questions:
+                        max_points += int(questions[question]['points'])
+                    for question in questions:
+                        await message.channel.send(question)
+                        def is_correct(m):
+                            return m.author == message.author
+                        answer = questions[question]['answer']
+                        try:
+                            guess = await self.wait_for('message', check=is_correct, timeout=30.0)
+                        except asyncio.TimeoutError:
+                            return await message.channel.send('No answer, no fun! You took too long...'.format(answer))
+                        if guess.content == answer:
+                            no_correct += 1
+                            no_points += int(questions[question]['points'])
+                    await message.channel.send("You answered correctly " + str(no_correct) + " questions of " + str(no_questions) + " and " + str(no_points) + " points of maximum " + str(max_points) + "!")
 
 client = MyClient()
 client.run(sys.argv[1])
