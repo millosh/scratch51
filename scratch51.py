6d02f535a18c19afc7f69bc22e72455542dd1239
Milos Rancic <millosh@gmail.com>
2022-05-07 19:12:31 +0200

    .

--- scratch51.py
+++ scratch51.py
@@ -1,16 +1,9 @@
-import discord, random, asyncio, json
-import random
-import asyncio
-
-qas = {
-    "question1": "a1",
-    "question2": "a2",
-    "question3": "a3",
-}
+import discord, random, asyncio, json, re
 
 root_dir = "."
 conf_dir = root_dir + "/config"
 actions_fd = conf_dir + "/actions.json"
+messages_fd = conf_dir + "/messages.org"
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -23,27 +16,32 @@ class MyClient(discord.Client):
         # we do not want the bot to reply to itself
         if message.author.id == self.user.id:
             return
-        
-        actions = json.load(actions.json)
-        if message.content.startswith('$quiz1'):
-            no_correct = 0
-            no_questions = len(list(qas.keys()))
-            for question in qas:
-                await message.channel.send(question)
-                def is_correct(m):
-                    return m.author == message.author
-                answer = qas[question]
-                try:
-                    guess = await self.wait_for('message', check=is_correct, timeout=30.0)
-                except asyncio.TimeoutError:
-                    return await message.channel.send('No answer, no fun! You took too long...'.format(answer))
-                if guess.content == answer:
-                    no_correct += 1
-            await message.channel.send("You answered correctly " + str(no_correct) + " questions of " + str(no_questions) + "!")
 
-        if message.content.startswith('$help'):
-            await message.channel.send("""$quiz(index) - Run a quiz you made.
-Expect further commands here. The developer is busy with what commands there shall be.""")
+        messages = re.split("^- ",open(messages_fd).read())[1:]
+        for message in messages:
+            trigger = message[0]
+            content = message[1:]
+            if message.content.startswith(trigger):
+                await message.channel.send(content)
+
+#        actions = json.load(actions.json)
+#        if message.content.startswith('$quiz1'):
+#            no_correct = 0
+#            no_questions = len(list(qas.keys()))
+#            for question in qas:
+#                await message.channel.send(question)
+#                def is_correct(m):
+#                    return m.author == message.author
+#                answer = qas[question]
+#                try:
+#                    guess = await self.wait_for('message', check=is_correct, timeout=30.0)
+#                except asyncio.TimeoutError:
+#                    return await message.channel.send('No answer, no fun! You took too long...'.format(answer))
+#                if guess.content == answer:
+#                    no_correct += 1
+#            await message.channel.send("You answered correctly " + str(no_correct) + " questions of " + str(no_questions) + "!")
+#
+#        if message.content.startswith('$help'):
 
 client = MyClient()
 client.run('OTY5MTkyNzc2MDI3NzU0NTA2.Ymp1HA.x9n4v_SfvLZC3Csc5Y7s21rvA3E')
